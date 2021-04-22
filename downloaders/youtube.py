c870b6b87b398a2550ff7f5dcd2ac8387a9935d5
Lonely Together <82939210+HalbertKun@users.noreply.github.com>
2021-04-22 15:54:44 +0700

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -20,12 +20,12 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Video lebih lama dari {DURATION_LIMIT} minute(s) tidak diizinkan, video yang disediakan adalah {duration} minute(s)"
+            f"❌ Durasi video lebih lama dari {DURATION_LIMIT} minute(s) tidak diperbolehkan, durasi video yang disediakan adalah {duration} minute(s)"
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"❌ Video lebih lama dari {DURATION_LIMIT} minute(s) tidak diizinkan, video yang disediakan adalah {duration} minute(s)"
+            f"❌ Durasi video lebih lama dari {DURATION_LIMIT} minute(s) tidak diperbolehkan, durasi video yang disediakan adalah {duration} minute(s)"
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
