1adea5d8c26c346aefce0319968868241fe150bb
Lonely Together <82939210+HalbertKun@users.noreply.github.com>
2021-04-27 16:35:19 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Alooo Ges 🤩 Saya adalah **Layanan Asisten Irama Musik Bot.**\n\n ❗️ Rules :\n   - Biasakan salam, amanat dari [Owner](t.me/IamYourEnemy) \n   - Jangan spam pemutaran lagu agar bot tidak error \n\n **Note :** Kirim link undangan grup atau username grup jika userbot/asisten tidak dapat bergabung dengan grup anda atau yang lainnya.\n\n 🛠️ Ke [Group Support](t.me/VcgSupportGroup) atau bisa PC ke owner [Hendra](t.me/IamYourEnemy)\n\n")
+  await USER.send_message(message.chat.id,"Alooo Ges 🤩 Saya adalah **Layanan Asisten Irama Musik Bot.**\n\n ❗️ Rules :\n   - Biasakan salam, amanat dari [Owner](t.me/IamYourEnemy) \n   - Jangan spam pemutaran lagu agar bot tidak error \n\n **Note :** Kirim link undangan grup atau username grup jika userbot/asisten tidak dapat bergabung dengan grup anda atau yang lainnya.\n\n 🛠️ Ke [Group Support](t.me/VcgSupportGroup) atau bisa PC ke owner @IamYourEnemy ⚡\n\n")
   return                        
