4bfc1529c96fbe12dfe566cc9db59fdbf7adc8c1
Red Devil <82816642+Naysilaa@users.noreply.github.com>
2021-05-04 17:16:03 +0700

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -1,3 +1,20 @@
+# Calls Music 1 - Telegram bot for streaming audio in group calls
+# Copyright (C) 2021  Roj Serbest
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from typing import Callable
 
 from pyrogram import Client
@@ -17,7 +34,7 @@ def errors(func: Callable) -> Callable:
     return decorator
 
 
-def authorized_users_only(func: Callable) -> Callable:
+def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         if message.from_user.id in SUDO_USERS:
             return await func(client, message)
