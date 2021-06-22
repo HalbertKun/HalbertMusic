b6156e8dba9d0d9942ca6a1593d33ce77db96f77
Lonely Together <82939210+HalbertKun@users.noreply.github.com>
2021-06-23 01:54:28 +0700

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -78,7 +78,7 @@ def song(client, message):
     except Exception as e:
         print(e)
 
-ARQ_API = "http://35.240.133.234:8000"
+ARQ_API = "EFNOBV-BNQVMY-YYRIWY-NLBMIL-ARQ"
 arq = ARQ(ARQ_API)
 
 
